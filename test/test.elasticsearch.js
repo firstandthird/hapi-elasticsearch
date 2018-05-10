c846281debe24741f5826176a03cea7bd3534849
Chris <orthagonal1@gmail.com>
2018-05-10 12:00:57 -0500

    update

--- test/test.elasticsearch.js
+++ test/test.elasticsearch.js
@@ -4,18 +4,20 @@ const plugin = require('../index.js');
 const ES = require('elasticsearch-mock-js');
 
 tap.test('can load plugin', async t => {
-  await ES.start();
-  const server = new Hapi.Server({ port: 8080 });
-  server.register({
-    plugin,
-    options: {
-      host: 'localhost:9200',
-      elasticLog: 'debug',
-    }
+  ES.start(async (err) => {
+    const server = new Hapi.Server({ port: 8080 });
+    server.register({
+      plugin,
+      options: {
+        host: 'localhost:9200',
+        elasticLog: 'debug',
+      }
+    });
+    await server.start();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    ES.stop(async (err) => {
+      await server.stop();
+      t.end();
+    });
   });
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  await server.start();
-  await ES.stop();
-  await server.stop();
-  t.end();
 });
